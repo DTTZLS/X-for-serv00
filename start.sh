ff4655105176a6b71712b8810ded59d15a947b43
DTTZLS <94920648+DTTZLS@users.noreply.github.com>
2024-07-25 16:50:50 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # 端口参数 （必填）
-export WEBPORT=
-export VMPORT=
+export WEBPORT=6281
+export VMPORT=38556
 
 # web.js 参数 （必填）
-export UUID=de04add9-5c68-8bab-950c-08cd5320df18
+export UUID=2a9f857f-c5f6-4061-b259-e40ea78f4e3c
 export WSPATH=serv00
 
 # ARGO 隧道参数（如需固定 ARGO 隧道，请把 ey 开头的 ARGO 隧道的 token 填入 ARGO_AUTH ，仅支持这一种方式固定，隧道域名代理的协议为 HTTP ，端口为 VMPORT 同端口。如果不固定 ARGO 隧道，请删掉ARGO_DOMAIN那行，保留ARGO_AUTH这行。）
-export ARGO_AUTH=''
-export ARGO_DOMAIN=
+export ARGO_AUTH='eyJhIjoiOTliOGQ3ZmQ3ODI2NjBjOTQ0ZjBjNzg4OTEyMzRmZTgiLCJ0IjoiMmE3N2MzYmItOTNiZi00YThjLTg2YmMtYjc2MTk5NjRmNmY5IiwicyI6IlpUSTFOek13WkRRdE9UWTNNaTAwTmpZekxUZ3hNR010WVdJd09HUXdOalkyT1RjeSJ9'
+export ARGO_DOMAIN=vs.comfyui.us.kg
 
 # 网页的用户名和密码（可不填，默认为 admin 和 password ，如果不填请删掉这两行）
-export WEB_USERNAME=
-export WEB_PASSWORD=
+export WEB_USERNAME=an
+export WEB_PASSWORD=960522
 
 # 启动程序
 USERNAME=$(whoami)
